d42bcddfaf8740c97d0787c104b03da5e91c1518
T-light-ux <abdulkareemnurudeen01@gmail.com>
2021-11-13 13:04:33 +0100

    task submission

---
+++ i.py
@@ -0,0 +1,16 @@
+numbers = "123456789"
+# return 'the odd number'
+# reverse indexing
+numbers[::-1]
+
+name = "MUSHRAH"
+myname = name.lower()
+print(myname)
+
+word = "phython is so cool"
+solve = word[-4:] *3
+print(solve)
+
+flower = "Rose"
+plant = flower[0]
+print(plant)
---
+++ task4.py
@@ -0,0 +1,62 @@
+#TASK FOUR (4)
+#return index (how old) from string hello there, how old are you?
+
+my_str = 'hello there, how old are you?'
+print(my_str[13:20])
+
+#story
+#return story[2:4]+story[-1]
+
+story='python is awesome'
+print(story[2:4] + story[-1])
+
+#what is len(mystring)
+#return len
+
+mystring ='python rocks'
+print(len(mystring))
+
+#flower='Rose'
+#flowe(0)='P'?
+#print(flower)
+
+"cannot return 'Pose' "
+
+word='python is cool'
+print(word[-4:] *3)
+
+#UPPERCASE
+newname='pepsi'
+newname.upper()
+
+#code that returns 'macdonald' as MacDonald
+name = 'macdonald'
+code = name[:3].capitalize() + name[3:].capitalize()
+print(f"{code}")
+
+#lowercase
+oldname='MUSHARAH'.lower()
+
+#built-in method to change string to list
+mystring = "Hello World"
+code = mystring.split()
+print(f" '{mystring.split()}")
+
+#Remove "Hello" from "Hello World"
+a="hello"
+b="world"
+print(f'{a+b}')
+print (f'{a}')
+
+#the index of the first character in a string is (o)
+
+#print out numbers of 'o' in the string "hello, python is sooo cool"
+my_str = "hello, python is sooo cool"
+amount = (my_str.count('o'))
+print(f"The number of 'o' in {my_str} is: {amount}")
+
+#convert this string to a title case "hello world! welcom to python"
+newcase="hello world! welcom to python".title ()
+print(newcase)
+
+
